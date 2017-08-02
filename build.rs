1d675f8073f10170f3a074f018052a89511991aa
bluss <bluss@users.noreply.github.com>
2017-08-02 14:48:36 +0200

    Use version_check to check rust version (compiles faster)

--- build.rs
+++ build.rs
@@ -3,16 +3,11 @@
 //! This build script detects if we are nightly or not
 //!
 
-extern crate rustc_version;
-
-use rustc_version::Channel;
+extern crate version_check;
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    match rustc_version::version_meta().channel {
-        Channel::Dev | Channel::Nightly => {
-            println!("cargo:rustc-cfg=use_nightly");
-        }
-        _otherwise => { }
+    if let Some(true) = version_check::supports_features() {
+        println!("cargo:rustc-cfg=use_nightly");
     }
 }
